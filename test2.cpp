dfaf0aaf4a9f538c87cc2cbcfa4fedf59643db4d
Khuong Pham <boyzsno1@gmail.com>
2024-07-29 00:31:56 +0700

    update problem E

--- test2.cpp
+++ test2.cpp
@@ -20,67 +20,6 @@ using namespace std;
 
 void solve()
 {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> grid(n, vector<int>(m));
-    FOR(i, n)
-    {
-        FOR(j, m)
-        cin >> grid[i][j];
-    }
-    vector<int> vR, vC;
-    for (int i = 0; i < n; i++)
-    {
-        int cnt = 0;
-        for (int j = 0; j < m; j++)
-        {
-            if (grid[i][j] == 1)
-                cnt++;
-        }
-        if (cnt >= 2)
-            vR.push_back(i);
-    }
-    for (int i = 0; i < m; i++)
-    {
-        int cnt = 0;
-        for (int j = 0; j < n; j++)
-        {
-            if (grid[j][i] == 1)
-                cnt++;
-        }
-        if (cnt >= 2)
-            vC.push_back(i);
-    }
-
-    map<string, bool> mp;
-    for (int i = 0; i < vR.size(); i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (grid[vR[i]][j] == 1)
-            {
-                string s = to_string(vR[i]) + to_string(j);
-                mp.insert(make_pair(s, true));
-            }
-        }
-    }
-    for (int i = 0; i < vC.size(); i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (grid[j][vC[i]] == 1)
-            {
-                string s = to_string(j) + to_string(vC[i]);
-                if (mp.find(s) == mp.end())
-                    mp.insert(make_pair(s, true));
-                else
-                    continue;
-            }
-        }
-    }
-    cout << mp.size();
-    // for (auto x : vC)
-    //     cout << x << " ";
 }
 
 int main()
--- test4.cpp
+++ test4.cpp
@@ -5,7 +5,7 @@
 #define FOR(i, n) for (int i = 0; i < (n); i++)
 #define REP(i, a, b) for (int i = (a); i <= (b); i++)
 #define vi vector<int>
-#define vsz v.size()
+#define vsz n
 #define ssz s.size()
 #define SZ(x) (x).size()
 #define ALL(x) (x).begin(), (x).end()
@@ -20,8 +20,38 @@ using namespace std;
 
 void solve()
 {
-    iii
-    
+    int n, m, k;
+    cin >> n >> m >> k;
+    string s;
+    cin >> s;
+    if (m > n)
+        yes else
+        {
+            vector<int> dp(n, 0);
+            // base
+            for (int i = 0; i < m; i++)
+            {
+                if (s[i] == 'W')
+                {
+                    if (s[i + 1] == 'L')
+                    {
+                        dp[i]++;
+                        dp[i + 1] += dp[i];
+                    }
+                    else if (s[i + 1] == 'W')
+                    {
+                        dp[i]++;
+                        if (s[i - 1] != 'C')
+                        {
+                            dp[i] += dp[i - 1];
+                        }
+                    }
+                }
+            }
+            for (auto x : dp)
+                cout << x << " ";
+            // exponent
+        }
 }
 
 signed main()
@@ -30,8 +60,9 @@ signed main()
     cin.tie(0);
     cout.tie(0);
 
-    // int t; cin >> t;
-    int t = 1;
+    int t;
+    cin >> t;
+    // int t = 1;
     while (t--)
     {
         solve();
